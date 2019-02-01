3b926393859ffb145fa79910b9a8ebac040e4061
everyback <543245543@qq.com>
2019-02-02 02:48:10 +0800

    将side与title组件提到app。vue 初步构建至login的全部跳转 未完成login的全局变量问题

---
+++ src/components/menuicon.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="box" :class="type==='menu' ?'':'bbox'">
+        <div class="top legt" :class="type==='menu' ?'':'btop'" ></div>
+        <div class="middum legt  " :class="type==='menu' ?'':'bmiddum'"></div>
+        <div class="buttom legt " :class="type==='menu' ?'':'bbuttom'"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "menuicon",
+        props:{
+            type:{
+                type:String,
+                default:"menu",
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+    .top{
+        transition: all 0.5s ease-in-out 0s;
+    }
+    .middum{
+        position: relative;
+        top: 8px;
+        transition: all 0.5s ease-in-out 0s;
+    }
+    .buttom {
+        position: relative;
+        top: 16px;
+        transition: all 0.5s ease-in-out 0s;
+    }
+    .legt{
+        width: 2.8rem;
+        height: 0.3rem;
+        border-radius: 0.1875rem;
+        background-color: #ffffff;
+    }
+    .box{
+        transform: scale(0.4);
+        margin-bottom: 0.5rem;
+    }
+    .bbox{
+        transform: scale(0.5);
+    }
+    .bmiddum
+    {
+        width: 2.3rem;
+        transform: rotate(-180deg) ;
+    }
+    .btop
+    {
+        width: 1.5625rem;
+        transform: rotate(-45deg) translateX(-0.45rem) translateY(0.122rem);
+    }
+    .bbuttom
+    {
+        width: 1.5625rem;
+        transform: rotate(45deg) translateX(-0.45rem) translateY(-0.122rem);
+    }
+</style>
--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,48 +1,48 @@
 <template>
     <div>
         <mu-container  >
-            <div style="width: calc(100% - 255px);height: 100%;position:fixed; z-index: 20141228;right:0; " v-swipeleft="sleft" v-if="!bigscreen&& sidebar.open" v-tap="sleft">
-            </div>
-            <div style="width: 2%;height: 80%;position:fixed;left:0;top:10%; z-index: 20141222 " v-swiperight="sright" v-if="!bigscreen&&!sidebar.open" >
-            </div>
-            <mu-drawer :open.sync="sidebar.open" :docked="bigscreen" :right="false"  :z-depth="0" style="width: 255px" v-swipeleft="sleft">
+            <v-touch style="width: calc(100% - 240px);height: 100%;position:fixed; z-index: 20141228;right:0; " v-if="!bigscreen&& sidebar.open" @swipeleft="sleft" @tap="sleft" >
+            </v-touch>
+            <v-touch style="width: 2%;height: 80%;position:fixed;left:0;top:10%; z-index: 20141222 " @swiperight="sright" v-if="!bigscreen&&!sidebar.open" :key="'side'" >
+            </v-touch>
+            <mu-drawer :open.sync="sidebar.open" :docked="bigscreen" :right="false"  :z-depth="0" style="width: 240px" v-swipeleft="(s,e)=>sleft('mus')"  >
                 <div v-if="bigscreen">
                     <mu-appbar  z-depth="0" color="lightBlue50" >
                         <span class="bigtest" slot="left">Heartbeat monitor</span>
                     </mu-appbar>
                 </div>
                 <mu-appbar  z-depth="0" >
-                        <span  slot="left" v-if="login">
-                            <img src="http://127.0.0.1:8000/api/avatar/-1"  style="max-width:5vh; max-height:5vw" />
-                        </span>
-                        <div slot="default" class="small-font" v-if="login">
-                            <p>
-                                <span>您好，<a href="#">{{user.name}}</a>！</span>
+                            <span  slot="left" v-if="login">
+                                <img src="http://127.0.0.1:8000/api/avatar/-1"  style="max-width:5vh; max-height:5vw" />
+                            </span>
+                    <div slot="default" class="small-font" v-if="login">
+                        <p>
+                            <span>您好，<a href="#">{{user.name}}</a>！</span>
 
-                            </p>
+                        </p>
 
-                        </div>
-                        <mu-button  v-if="login" slot="right" flat color="primary" small @click="logout()" >Logout</mu-button>
+                    </div>
+                    <mu-button  v-if="login" slot="right" flat color="primary" small @click="logout()" >Logout</mu-button>
                     <div v-if="!login">
                         <p><a href="/login">未登录</a></p>
                     </div>
                     <mu-button  v-if="!login" slot="right" flat color="primary" small @click="tologin()" >请登录</mu-button>
                 </mu-appbar>
                 <mu-divider />
-<!--                <mu-list>
-                    <mu-slide-left-transition>
-                        <div v-show="shows">
-                            <mu-list-item  @click="jumpto(key)" v-for="(item,key) in lists" :key="key"   button>
-                                <mu-list-item-title>{{item}}</mu-list-item-title>
-                            </mu-list-item>
-                            <mu-list-item  v-if="!this.$store.state.table" @click="muclose()" button>
-                                <mu-list-item-title>Close</mu-list-item-title>
-                            </mu-list-item>
-                        </div>
-                    </mu-slide-left-transition>
-                </mu-list>-->
+                <!--                <mu-list>
+                                    <mu-slide-left-transition>
+                                        <div v-show="shows">
+                                            <mu-list-item  @click="jumpto(key)" v-for="(item,key) in lists" :key="key"   button>
+                                                <mu-list-item-title>{{item}}</mu-list-item-title>
+                                            </mu-list-item>
+                                            <mu-list-item  v-if="!this.$store.state.table" @click="muclose()" button>
+                                                <mu-list-item-title>Close</mu-list-item-title>
+                                            </mu-list-item>
+                                        </div>
+                                    </mu-slide-left-transition>
+                                </mu-list>-->
                 <mu-list>
-                    <mu-list-item  v-if="bigscreen" @click="closebar()" button>
+                    <mu-list-item  v-if="!bigscreen" @click="closebar()" button>
                         <mu-list-item-title>Close</mu-list-item-title>
                     </mu-list-item>
                 </mu-list>
@@ -55,6 +55,7 @@
 <script>
 
     import Mydialog from "../template/mydialog";
+    import {mapActions, mapGetters} from 'vuex'
 
     export default {
         name: "sidebar",
@@ -85,64 +86,47 @@
             }
         },
         computed :{
-            bigscreen(){
-                //console.log(val);
-                return this.$store.state.screen > 4/3;
-            },
-            baropen()
-            {
-                return this.$store.state.sidebarOpen;
-            },
-            islogin()
-            {
-                return localStorage.getItem('login');
-            },
-
+            ...mapGetters(["bigscreen","islogin","barOpen"]),
         },
         watch:{
             bigscreen(val){
-                //console.log(val);
                 this.sidebar.open = val ;
-
             },
-/*            'sidebar.open':function (){
-                this.$store.commit('')
-            }*/
-            baropen()
+            barOpen()
             {
                 this.sidebar.open = this.$store.state.sidebarOpen;
             },
             'sidebar.open': {
                 handler(val)
                 {
+                    //if(!( val === barOpen))
                     this.$store.commit('newdata',{key:'sidebarOpen',data:this.sidebar.open});
                 }
             },
             islogin(val)
             {
-              this.login = !!val;
+                this.login = !!val;
             },
         },
         beforeMount()
         {
-          if (!this.bigscreen)
-          {
-              this.sidebar.open = false;
-          }
-          if (localStorage.getItem('login') === '1' )
-          {
-              this.login = true;
-              let s = JSON.parse(localStorage.getItem('user'));
-              //console.log(s.name);
-              this.user.name = s.name;
-              this.user.uid  = s.id;
-          }
-         //   console.log(localStorage.getItem('ssss'));
+            if (!this.bigscreen)
+            {
+                this.sidebar.open = false;
+            }
+            if (localStorage.getItem('login') === '1' )
+            {
+                this.login = true;
+                let s = JSON.parse(localStorage.getItem('user'));
+                //console.log(s.name);
+                this.user.name = s.name;
+                this.user.uid  = s.id;
+            }
         },
         methods:{
             closebar()
             {
-               this.sidebar.open = false;
+                this.sidebar.open = false;
             },
             logout()
             {
@@ -153,14 +137,16 @@
             },
             tologin()
             {
-              this.$router.push('/login');
+                this.$router.push('/login');
             },
             jumpto(val)
             {
                 this.$store.commit('newdata',{key:'title_name',data:val.name});
             },
-            sleft()
+            sleft(s,e)
             {
+                if (s==="mus")
+                    console.log("由本体出发");
                 if (this.bigscreen)
                     console.log("左滑触发,zhuomian");
                 else
@@ -170,9 +156,10 @@
                 }
 
             },
-            sright()
+            sright(s)
             {
                 this.sidebar.open = true;
+                console.log("右滑触发");
             }
 
         }
